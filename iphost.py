9c904c287b32f975e1cbd7616656d85518d393a7
Yur4ik12345 <gajdaryura@gmail.com>
2023-10-31 21:32:03 +0200

    change iphost.py

--- iphost.py
+++ iphost.py
@@ -2,4 +2,4 @@ myfile = open ("ip.txt","rt")
 contents = myfile.readlines()
 
 for line in contents:
-    print (line.replace("\n","")+ " ansible_become=yes ansible_ssh_user=adminuser ansible_ssh_private_key_file=~/.ssh/id_rsa ansible_python_interpreter=/usr/bin/python3")
+    print (line.replace("\n","")+ " ansible_become=yes ansible_ssh_user=adminuser ")
