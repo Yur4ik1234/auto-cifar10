69497e4e074c10d58322239485354fa74011c284
Yur4ik12345 <gajdaryura@gmail.com>
2023-11-28 15:13:50 +0200

    add to main.tf

--- main.tf
+++ main.tf
@@ -207,3 +207,72 @@ storage_profile_image_reference {
     }
   }
 }
+
+resource "azurerm_monitor_autoscale_setting" "auto" {
+  name                = "myAutoscaleSetting"
+  resource_group_name = azurerm_resource_group.res-1.name
+  location            = azurerm_resource_group.res-1.location
+  target_resource_id  = azurerm_virtual_machine_scale_set.vmss-1.id
+
+  profile {
+    name = "Weekends"
+
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 10
+    }
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_virtual_machine_scale_set.vmss-1.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 90
+      }
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "2"
+        cooldown  = "PT1M"
+      }
+    }
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_virtual_machine_scale_set.vmss-1.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 10
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "2"
+        cooldown  = "PT1M"
+      }
+    }
+
+    recurrence {
+      timezone  = "Pacific Standard Time"
+      days      = ["Saturday", "Sunday"]
+      hours     = [12]
+      minutes   = [0]
+    }
+  }
+
+  notification {
+    email {
+      send_to_subscription_administrator    = true
+      send_to_subscription_co_administrator = true
+      custom_emails                         = ["admin@contoso.com"]
+    }
+  }
+}
