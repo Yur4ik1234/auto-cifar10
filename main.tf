cf6e197368a4910ea735162a8264d84cb752037d
Yur4ik12345 <gajdaryura@gmail.com>
2023-12-05 10:34:51 +0200

    work

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
     storage_account_name = "yurahaidar"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    #use_oidc             = true
+    
   }
 }
 provider "azurerm" {
